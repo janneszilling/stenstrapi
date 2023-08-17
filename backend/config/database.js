58cd29d52754315dfc43ec04301989cc3812ff4e
Jannes Zilling <jannes.zilling@gmail.com>
2023-08-17 21:38:27 +0200

    database connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,21 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "mysql",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "strapi"),
+      uswer: env("DATABASE_USERNAME", "root"),
+      password: env("DATABASE_PASSWORD", "root"),
     },
-    useNullAsDefault: true,
   },
+  // connection: {
+  //   client: 'sqlite',
+  //   connection: {
+  //     filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+  //   },
+  //   useNullAsDefault: true,
+  // },
 });
