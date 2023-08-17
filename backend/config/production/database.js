da4c94f2460da0f97b0fca24b5b047946be3ad8c
Jannes Zilling <jannes.zilling@gmail.com>
2023-08-17 22:17:42 +0200

    Update database.js

--- backend/config/production/database.js
+++ backend/config/production/database.js
@@ -18,3 +18,24 @@
 //     },
 //   };
 // };
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: env("DATABASE_CLIENT", "mysql"),
+    connection: {
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "strapi"),
+      uswer: env("DATABASE_USERNAME", "root"),
+      password: env("DATABASE_PASSWORD", "root"),
+    },
+    useNullAsDefault: true,
+  },
+  // connection: {
+  //   client: 'sqlite',
+  //   connection: {
+  //     filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+  //   },
+  //   useNullAsDefault: true,
+  // },
+});
